b8060168975b4401d9d3ed3417fc956df606e4c7
Brianna Renni <briannaxrene@icloud.com>
2023-10-31 04:07:43 -0700

    Finish toggle.js

--- js/toggle.js
+++ js/toggle.js
@@ -1,30 +1,59 @@
 let toggleBtn = document.querySelector('.toggle-btn');
-let toggleBtnDark = document.querySelector('.dark-btn');
-let toggleBtnLight = document.querySelector('.light-btn');
+let toggleBtnDark = document.querySelector('#dark-btn');
+let toggleBtnLight = document.querySelector('#light-btn');
 let searchInput = document.querySelector('.search-bar');
 let profileCard = document.querySelector('.profile-card');
 let deskContainer = document.querySelector('.desk-container');
+let currentTheme = '';
+let isLargeScreen = window.matchMedia('(min-width:1024px)').matches;
+let screenWidth = window.innerWidth;
 
-const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+// Handle box shadow responsively
+const removeShadow = (el) => (el.style.boxShadow = 'none');
+const addShadow = (el) => (el.style.boxShadow = '0px 16px 30px -10px rgba(70, 96, 187, 0.2)');
 
-if (prefersDark) {
-  document.documentElement.setAttribute('data-theme', 'dark');
-  toggleBtnDark.classList.add('hidden');
-  toggleBtnLight.classList.remove('hidden');
-} else {
-  document.documentElement.setAttribute('data-theme', 'light');
-  toggleBtnDark.classList.remove('hidden');
-  toggleBtnLight.classList.add('hidden');
-}
-
-const removeShadow = (el) => {
-  el.classList.toggle('no-box-shadow');
+const responsiveShadow = () => {
+  if (isLargeScreen && currentTheme === 'light') {
+    addShadow(deskContainer);
+    removeShadow(profileCard);
+  } else if (screenWidth <= 1024 && currentTheme === 'light') {
+    addShadow(profileCard);
+    removeShadow(deskContainer);
+  }
 };
 
-const currentTheme = document.documentElement.getAttribute('data-theme');
+// Handle theme toggle
+const setLightTheme = () => {
+  document.documentElement.setAttribute('data-theme', 'light');
+  currentTheme = 'light';
+  toggleBtnDark.style.display = '';
+  toggleBtnLight.style.display = 'none';
+  addShadow(searchInput);
+  responsiveShadow();
+};
 
-if (currentTheme === 'dark') {
+const setDarkTheme = () => {
+  document.documentElement.setAttribute('data-theme', 'dark');
+  currentTheme = 'dark';
+  toggleBtnLight.style.display = '';
+  toggleBtnDark.style.display = 'none';
   removeShadow(searchInput);
   removeShadow(profileCard);
   removeShadow(deskContainer);
-}
+};
+
+const toggleScheme = () => {
+  currentTheme === 'dark' ? setLightTheme() : setDarkTheme();
+};
+
+// Check user pref & update theme on page load
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+prefersDark ? setDarkTheme() : setLightTheme();
+
+// Event handlers
+toggleBtnDark.addEventListener('click', toggleScheme);
+toggleBtnLight.addEventListener('click', toggleScheme);
+window.addEventListener('resize', () => {
+  isLargeScreen = window.matchMedia('(min-width:1024px)').matches;
+  responsiveShadow();
+});
